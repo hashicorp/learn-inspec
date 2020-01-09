1f98e6b42b2a63d5461ed1890c21adcc5f4d4ad8
Zack Smith <acidprime@wallcity.org>
2020-01-09 11:16:36 -0800

    Remove comments

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-# ---------------------------------------------------------------------------------------------------------------------
 # BASIC TERRAFORM EXAMPLE
-# See test/terraform_aws_example.go for how to write automated tests for this code.
-# ---------------------------------------------------------------------------------------------------------------------
 
 data "template_file" "example" {
   template = "${var.example}"
