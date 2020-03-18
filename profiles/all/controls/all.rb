70047e5f00a466bd47c43ade1385647f704b2e52
Zack Smith <acidprime@wallcity.org>
2020-03-18 01:57:40 -0700

    DEBUG

--- profiles/all/controls/all.rb
+++ profiles/all/controls/all.rb
@@ -1,6 +1,4 @@
-
-puts "system('ls -l /github')"
-puts "system('ls -l /markdown')"
+ raise "No markdown files found! #{Dir.glob("/markdown/*").inspect}\n#{Dir.glob("/github/*").inspect}" 
 include_controls 'vault'
 include_controls 'terraform'
 include_controls 'consul'
