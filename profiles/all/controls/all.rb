d314514280ad4d1d5c80681aa04df39f16ad2c14
Zack Smith <acidprime@wallcity.org>
2020-03-18 13:28:35 -0700

    Revert "Switch to realtive"
    This reverts commit 4e0083bbe9eb472ed2a1d1a95fbff60ccad9c5e1.

--- profiles/all/controls/all.rb
+++ profiles/all/controls/all.rb
@@ -1,3 +1,4 @@
+ raise "No markdown files found! #{Dir.glob("/markdown/*").inspect}\n#{Dir.glob("/github/*").inspect}" 
 include_controls 'vault'
 include_controls 'terraform'
 include_controls 'consul'
